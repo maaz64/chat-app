7d80d7e2dafe3687394a760642c502cd0687d92c
maazwealthy <abu.maaz@wealthy.in>
2025-05-06 18:18:50 +0530

    fix rendering issue

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/chat-app">
+        <Route path="/">
           <Route
             index
             element={
--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDCrHT00ipKluIy3GSwygyf0JlojuNIcwY",
-  authDomain: "chat-app-e42e4.firebaseapp.com",
-  projectId: "chat-app-e42e4",
-  storageBucket: "chat-app-e42e4.appspot.com",
-  messagingSenderId: "974426538878",
-  appId: "1:974426538878:web:dc9188ff77b5102a95e92f"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
